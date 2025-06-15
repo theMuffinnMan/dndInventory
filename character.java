5c5fa77995eab3e53014b3d600bccc9cfbc250ce
theMuffinnMan <164093037+theMuffinnMan@users.noreply.github.com>
2025-06-16 09:43:58 +1200

    Character class instance variables

---
+++ character.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+/**
+ * Instance of a character.
+ * Stores all of the characters values and inventory hashmap.
+ *
+ * @Fleur
+ * @version (a version number or a date)
+ */
+public class character
+{
+    // instance variables - replace the example below with your own
+    private int health;
+    private int strength;
+    private int intellegence;
+    
+    private int gold;
+    
+    
+
+    /**
+     * Constructor for objects of class character
+     */
+    public character()
+    {
+        // initialise instance variables
+        ;
+    }
+
+}
