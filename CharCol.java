018f670e2d09e23d6b00d5fa5e9028b78b6de05a
theMuffinnMan <164093037+theMuffinnMan@users.noreply.github.com>
2025-09-09 12:29:11 +1200

    CheckStyle validation and minor bug fixes

--- CharCol.java
+++ CharCol.java
@@ -4,53 +4,54 @@ import java.util.HashMap;
  * Manages and changes information about Character and Item.
  *
  * @Fleur
- * @17/6
+    17/6
  */
-public class CharCol
-{
-    // instance variables - replace the example below with your own
-    private HashMap<Integer, Character> characterCollection;
-    private int currCharId;
-    private Character currChar;
 
-    /**
-     * Constructor for objects of class CharCol
-     */
-    public CharCol()
-    {
-        // initialise instance variables
-        characterCollection = new HashMap<Integer, Character>();
-        
-        //creates characters for collection
-        Character c1 = new Character("Nadja", "img/charPlaceHold.jpg", 24, 15, 8, 25, 
-        "Symbol of Urden", "img/urdenPlaceHold.jpg", "Plate Armour", "img/armourPlaceHold.jpg", "Bell Greatsword", "img/greatswordPlaceHold.jpg");
-        Character c2 = new Character("Micah", "img/micah.jpg", 20, 10, 16, 35,
-        "Fireball Helmet", "img/helmet.jpg", "Catalyst", "img/catalyst.jpg", "Moonbeam", "img/moonbeam.jpg");
-        
-        //puts character in collection
-        this.characterCollection.put(1, c1);
-        this.characterCollection.put(2, c2);
-        
-        //sets current character
-        this.currCharId = 1;
-        this.currChar = characterCollection.get(currCharId);
-        
-    }
+public class CharCol {
+  // instance variables - replace the example below with your own
+  private HashMap<Integer, Character> characterCollection;
+  private int currCharId;
+  private Character currChar;
+
+  /**
+   * Constructor for objects of class CharCol.
+   */
+  public CharCol() {
+    // initialise instance variables
+    characterCollection = new HashMap<Integer, Character>();
     
-    /**
-     * swap cahracters
-     */
-    public void swapCharacter(int id){
-        //set current character id 
-        this.currCharId = id;
-        this.currChar = characterCollection.get(currCharId);
-    }
+    //creates characters for collection
+    Character c1 = new Character("Nadja", "img/charPlaceHold.jpg", 24, 15, 8, 25, 
+        "Symbol of Urden", "img/urdenPlaceHold.jpg", "Plate Armour", 
+        "img/armourPlaceHold.jpg", "Bell Greatsword", "img/greatswordPlaceHold.jpg");
+    Character c2 = new Character("Micah", "img/micah.jpg", 20, 10, 16, 35,
+        "Fireball Helmet", "img/helmet.jpg", "Catalyst", "img/catalyst.jpg", 
+        "Moonbeam", "img/moonbeam.jpg");
     
+    //puts character in collection
+    this.characterCollection.put(1, c1);
+    this.characterCollection.put(2, c2);
     
-    /**
-     * reuturns current character values.
-     */
-    public Character getCharacter(){
-        return currChar;
-    }
+    //sets current character
+    this.currCharId = 1;
+    this.currChar = characterCollection.get(currCharId);
+    
+  }
+  
+  /**
+   * Swap cahracters.
+   */
+  public void swapCharacter(int id) {
+    //set current character id 
+    this.currCharId = id;
+    this.currChar = characterCollection.get(currCharId);
+  }
+  
+  
+  /**
+   * Reuturns current character values.
+   */
+  public Character getCharacter() {
+    return currChar;
+  }
 }
--- Character.java
+++ Character.java
@@ -1,175 +1,186 @@
-import java.util.HashMap;
 import java.util.ArrayList;
-import ecs100.*;
+import java.util.HashMap;
 /**
  * Instance of a character.
  * Stores all of the characters values and inventory HashMap.
  *
  * @Fleur
- * @version (a version number or a date)
+    17/6
  */
-public class Character
-{
-    // instance variables - replace the example below with your own
-    private String name;
-    private String image;
-    private int health;
-    private int strength;
-    private int intellegence;
-    
-    private int gold;
-    
-    private ArrayList<Item> inventory;
+
+public class Character {
+  // instance variables - replace the example below with your own
+  private String name;
+  private String image;
+  private int health;
+  private int strength;
+  private int intellegence;
+  
+  private int gold;
+  
+  private ArrayList<Item> inventory;
  
-    private Item currItem;
-    
-    private Item handItem;
+  private Item currItem;
+  
+  private Item handItem;
 
-    /**
-     * Constructor for objects of class character
-     */
-    public Character(String nm, String img, int hp, int str, int intel, int gp, 
-                    String item1Name, String item1Img, String item2Name, String item2Img, String itemHandName, String item3Img)
-    {
-        // initialise instance variables
-        name = nm;
-        image = img;
-        health = hp;
-        strength = str;
-        intellegence = intel;
-        gold = gp;
-        
-        //inventory 
-        inventory = new ArrayList< Item>();
-        //item creation
-        Item i1 = new Item(item1Name, item1Img, 0);
-        Item i2 = new Item(item2Name, item2Img, 1);
-        //place items into HashMap
-        this.inventory.add(i1);
-        this.inventory.add(i2);
-        
-        // set hand item
-        Item i3 = new Item(itemHandName, item3Img, 2);
-        this.handItem = i3;
-        
-    }
+  /**
+   * Constructor for objects of class character.
+   */
+  public Character(String nm, String img, int hp, int str, int intel, int gp, 
+          String item1Name, String item1Img, String item2Name, String item2Img, 
+          String itemHandName, String item3Img) {
+    // initialise instance variables
+    name = nm;
+    image = img;
+    health = hp;
+    strength = str;
+    intellegence = intel;
+    gold = gp;
+    
+    //inventory 
+    inventory = new ArrayList<Item>();
+    //item creation
+    Item i1 = new Item(item1Name, item1Img, 0);
+    Item i2 = new Item(item2Name, item2Img, 1);
+    //place items into HashMap
+    this.inventory.add(i1);
+    this.inventory.add(i2);
+    
+    // set hand item
+    Item i3 = new Item(itemHandName, item3Img, 2);
+    this.handItem = i3;
+    
+  }
 
-    /**
-     * hand item setter
-     */
-    public void setHand(){
-        
-        this.handItem.setId(this.currItem.getId());
-        
-        //remove current item from inventory
-        this.inventory.set(this.currItem.getId(), this.handItem);
-        
-        //set hand item to hand
-        this.handItem = this.currItem;
-        
-        this.currItem = this.inventory.get(this.handItem.getId());
-    }
+  /**
+   * Hand item setter.
+   */
+  public void setHand() {
     
-    /**
-     * current item setter
-     */
-    public void setCurrItem(Item newItem){
-        //sets a current item
-        this.currItem = newItem;
-    }
+    this.handItem.setId(this.currItem.getId());
     
-    /**
-     * current item getter
-     */
-    public Item getCurrItem(){
-        //sets a current item
-        return this.currItem;
-    }
+    //remove current item from inventory
+    this.inventory.set(this.currItem.getId(), this.handItem);
     
-    /**
-     * hand item getter
-     */
+    //set hand item to hand
+    this.handItem = this.currItem;
     
-    public Item getHand(){
-        return handItem;
-    }
-    /**
-     * name getter
-     */
-    public String getName(){
-        //returns gold value
-        return name;
-    }
-    /**
-     * image getter
-     */
-    public String getImage(){
-        //returns image file name
-        return image;
-    }
-    /**
-     * health getter
-     */
-    public int getHp(){
-        //returns health value
-        return health;
-    }
-    
-    /**
-     * strength getter
-     */
-    public int getStrength(){
-        //returns strength value
-        return strength;
-    }
-    
-    /**
-     * intellegence getter
-     */
-    public int getIntel(){
-        //returns intellegence value
-        return intellegence;
-    }
-    
-    /**
-     * gold getter
-     */
-    public int getGold(){
-        //returns gold value
-        return gold;
-    }
-    
-    /**
-     * gold setter add
-     */
-    public void addGold(){
-        //add one gold
-        this.gold++;
-    }
+    this.currItem = this.inventory.get(this.handItem.getId());
+  }
+  
+  /**
+   * Current item setter.
+   */
+  
+  public void setCurrItem(Item newItem) {
+    //sets a current item
+    this.currItem = newItem;
+  }
+  
+  /**
+   * Current item getter.
+   */
+  
+  public Item getCurrItem() {
+    //sets a current item
+    return this.currItem;
+  }
+  
+  /**
+   * Hand item getter.
+   */
+  
+  public Item getHand() {
+    return handItem;
+  }
+  /**
+   * Name getter.
+   */
 
-    /**
-     * gold setter minus
-     */
-    public void takeGold(){
-        //check if gold is 0 or lower
-        if(this.gold >= 1){
-            //take one gold
-            this.gold--;
-        }
-    }
-    
-    /**
-     * gold setter
-     */
-    public void editGoldSl(int newGold){
-        this.gold = newGold;
-    }
+  public String getName() {
+    //returns gold value
+    return name;
+  }
+  /**
+   * Image getter.
+   */
+  
+  public String getImage() {
+    //returns image file name
+    return image;
+  }
+  /**
+   * Health getter.
+   */
+  
+  public int getHp() {
+    //returns health value
+    return health;
+  }
+  
+  /**
+   * Strength getter.
+   */
+  
+  public int getStrength() {
+    //returns strength value
+    return strength;
+  }
+  
+  /**
+   * Intellegence getter.
+   */
+  
+  public int getIntel() {
+    //returns intellegence value
+    return intellegence;
+  }
+  
+  /**
+   * Gold getter.
+   */
+  
+  public int getGold() {
+    //returns gold value
+    return gold;
+  }
+  
+  /**
+   * Gold setter add.
+   */
+  
+  public void addGold() {
+    //add one gold
+    this.gold++;
+  }
 
-    /**
-     * inventory getter
-     */
-    public ArrayList< Item> getInv(){
-        //returns inventory HashMap
-        return inventory;
+  /**
+   * Gold setter minus.
+   */
+  
+  public void takeGold() {
+    //check if gold is 0 or lower
+    if (this.gold >= 1) {
+      //take one gold
+      this.gold--;
     }
+  }
+  
+  /**
+   * Gold setter.
+   */
+  
+  public void editGoldSl(int newGold) {
+    this.gold = newGold;
+  }
+
+  /**
+   * Inventory getter.
+   */
+  
+  public ArrayList<Item> getInv() {
+    //returns inventory HashMap
+    return inventory;
+  }
 }
